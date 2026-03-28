57673286b27ab120afa3f4aa9b3a32b731ce3f47
John Demirci <ucjdemirci@gmail.com>
2026-03-27 17:42:46 -0700

    Initial Commit

---
+++ linkedinME/linkedinMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkedinMEApp.swift
+//  linkedinME
+//
+//  Created by John Demirci on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct linkedinMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ linkedinMETests/linkedinMETests.swift
@@ -0,0 +1,17 @@
+//
+//  linkedinMETests.swift
+//  linkedinMETests
+//
+//  Created by John Demirci on 3/27/26.
+//
+
+import Testing
+@testable import linkedinME
+
+struct linkedinMETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
